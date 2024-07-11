6f33e17ca3d8d6a775ee35c88f959827c522111c
w1_liamby <weiliang_bai@foxmail.com>
2024-07-11 11:30:01 +0800

    update

---
+++ .script/launch-edge.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# launch edge on Hyprland.
+# ref: [Solved] Microsoft Edge blurry display/scaling issue on Hyperland.
+/usr/bin/microsoft-edge-stable --enable-features=UseOzonePlatform --ozone-platform=wayland --enable-wayland-ime "$@"
